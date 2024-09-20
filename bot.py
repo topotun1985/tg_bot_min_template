75119f691aaacc4950f37cbab0edbe7966cc2a58
topotun1985 <39965148+topotun1985@users.noreply.github.com>
2024-09-20 18:20:03 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,4 +56,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        logging.error("Бот був вимкнений!")
+        logging.error("Бот был выключен!")
